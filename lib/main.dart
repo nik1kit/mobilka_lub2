b1b0e6548ca25c9de40a20df5d5da6e32ace2f03
nik1kit <49343348+nik1kit@users.noreply.github.com>
2025-12-12 23:01:01 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'screens/weather_screen.dart';
+import 'business_logic/weather_controller.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider(
+      create: (_) => WeatherController(),
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Weather App',
+        theme: ThemeData(primarySwatch: Colors.blue),
+        home: const WeatherScreen(),
+      ),
+    );
+  }
+}
 
